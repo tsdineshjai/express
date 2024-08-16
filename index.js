43f1a630c06fe4f4f93e66bde45d122a87089277
D JAI <tsdineshjai@gmail.com>
2024-08-16 22:35:01 +0530

    updates

--- index.js
+++ index.js
@@ -2,30 +2,23 @@
 
 const book = require("./routes/book");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+// server.get("/", (req, res) => res.send("https secure"));
 app.set("view engine", "ejs");
-
 app.use("/books", book);
 
 // app.set("views", __dirname + "/views");
-
 app.get("/content", (req, res) => {
 	res.render("index", { tries: "successEveryTime" });
 });
 
-const callbackOne = (req, res, next) => {
-	res.send("hello");
-	console.log(`this is callback functtion one`);
-	next();
-};
-const callbackTwo = (req, res, next) => {
-	console.log(`this is callback functtion two`);
-	next();
-};
-
-app.get("/", callbackOne, callbackTwo);
+app.get("/", (req, res) => {
+	res.send(`this is the base URL :: ${req.baseUrl} + "\\"`);
+});
 
 app.get("/ab?cd", (req, res) => {
 	res.send("ab?cd");
@@ -46,13 +39,15 @@ app.get("/path/:userId/:name.:plant", (req, res) => {
 	res.send(req.params);
 });
 
+/* good case to pass on the param as a property to the request object.  */
 app.get("/contact/:name", (req, res) => {
-	res.send(`contacted the person : ${req.params.name} `);
+	console.log(req.baseUrl);
+	res.send(`contacted the person : ${req.myName} `);
 });
 
 app.param("name", (req, res, next, nameId) => {
 	if (nameId == "tsri") {
-		req.myName = nameId;
+		req.myName = "D Tanishka Sri";
 		next();
 	}
 });
--- routes/book.js
+++ routes/book.js
@@ -2,11 +2,34 @@ const express = require("express");
 
 const book = express.Router();
 
+book.all("/", (req, res) => res.send("you can do it"));
+
 book.get("/module", (req, res) => {
-	res.send("This is router module");
+	console.log(req.route);
+
+	res.render("some");
 });
 book.get("/hello", (req, res) => {
 	res.send("This is router hello");
 });
+book.get("/hello/:secret", (req, res) => {
+	console.dir(req.protocol);
+	res.send(`The secret is ${req.topvaluSecret}`);
+});
+
+book.param("secret", (req, res, next, value) => {
+	if (value == "relentless") {
+		req.topvaluSecret = value;
+		next();
+	}
+});
 
 module.exports = book;
+
+
+res.set({
+
+	"Content-Type" : "application/json || text/html | text/plain",
+
+	"Content-Length" : "123 "
+})
