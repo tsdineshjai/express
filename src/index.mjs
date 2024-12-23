7fd31b30818ca062bc59fa438cdcbf483bd1b476
D JAI <tsdineshjai@gmail.com>
2024-12-23 17:29:22 +0800

    updates

--- src/index.mjs
+++ src/index.mjs
@@ -2,16 +2,22 @@
 
 import express from "express";
 
+import { db } from "./router/db.js";
+
 const app = express();
 
 const Port = process.env.PORT || 3000;
+
+app.use("/db", db);
 
 app.get("/", (req, res) => {
 	res.send(`this is the port i am using ${process.env.PORT}`);
 });
 
+app.post("/", (req, res) => {
+	res.send("You just called the post method at '/hello'! ");
+});
+
 app.listen(Port, (req) => {
-	console.log(process.env.PORT);
-	console.log(process.env.NODE_ENV);
 	console.log(`listening to the port ${Port}`);
 });
